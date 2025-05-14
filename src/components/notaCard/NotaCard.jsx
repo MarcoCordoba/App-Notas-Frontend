4c3e6438ec01b2be690f63dae0db44f5f8484aa5
Marco Córdoba <marco.cba2@gmail.com>
2025-05-14 01:17:48 -0300

    cambio de titulo y imagen de la app

--- src/components/notaCard/NotaCard.jsx
+++ src/components/notaCard/NotaCard.jsx
@@ -2,9 +2,6 @@ import { useNavigate } from "react-router-dom";
 
 //este archivo es para mostrar solo ciertas partes de la tarea en el home 
 
-
-
-
 export function NotaCard({tarea}){
     
     const navigate = useNavigate();
