e7c6d284061fb2436bc9f088fbe6c5dca1e0844b
Marco Córdoba <marco.cba2@gmail.com>
2025-06-10 00:05:14 -0300

    logica de ocultar boton de logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,29 @@
 import './Navbar.css'
 import { LogOut } from 'lucide-react';
 import {Button} from "../button/Button"
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { logoutUser } from "../../api/AuthApi";
 
 export function Navbar() {
+
     const navigate = useNavigate();
+    const location = useLocation();//esto me guarda la ruta en la que estoy parado
+    
     const handleLogout = () => {
         logoutUser();
         navigate("/login");
     }
 
+    const hideLogout = location.pathname === "/login" || location.pathname === "/register";
+    //si el hidelogout es true es que estoy en /login o /registro
+    
     return (
         <div className='navbar'>
             <h2>NotasWeb</h2>
             <div className='boton-navbar'>
-                <Button className='boton-nav' onClick={handleLogout} > <LogOut /> </Button>
+                {!hideLogout && (// si es true no lo muestro en caso contrario muestro el boton, por eso !hidelogout
+                    <Button className='boton-nav' onClick={handleLogout} > <LogOut /> </Button>
+                )}    
             </div>
         </div>
     );
